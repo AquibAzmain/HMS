f529eddf845abadf25f42cdeef5da35cebb033c1
DESKTOP-7B10SV1\Aquib <bsse0718@iit.du.ac.bd>
2018-10-04 17:40:19 +0600

    student details update

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -22,6 +22,10 @@ export const StudentRoutes: Routes = [
   path: "details/:id", 
   component: StudentDetailComponent,
   pathMatch: 'full',
+  data: {
+    breadcrumb: 'ছাত্র বিবরণ',
+    icon: 'icofont icofont-id bg-c-pink'
+  }
 }];
 
 @NgModule({
