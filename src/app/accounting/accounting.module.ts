7ef08741c9a6ffe24ed8eb2c3ec3af80859b0427
Aquib Azmain <bsse0718@iit.du.ac.bd>
2018-10-07 01:23:02 +0600

    accounting app added

---
+++ src/app/accounting/accounting.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IncomeComponent } from './income/income.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [IncomeComponent]
+})
+export class AccountingModule { }
