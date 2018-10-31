9311f2b4f2049c6465019cae6777f5556db4a2c0
Moumita Asad <bsse0731@iit.du.ac.bd>
2018-10-31 13:10:06 +0600

    notification modified

--- src/app/notifications/notification.service.ts
+++ src/app/notifications/notification.service.ts
@@ -5,6 +5,7 @@ import { Notification } from '../../models/Notification';
 import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class NotificationService {
+  token = localStorage.getItem('token');
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json'})
   };
@@ -13,26 +14,28 @@ export class NotificationService {
 
   getNotification() : Observable<Notification> {
     return this.http.post<Notification>(Server.API_ENDPOINT +'getNotification/', {
-      token: "mew"
+      token: this.token
+    },this.httpOptions)
+  }
+
+  getNotificationSize() {
+    return this.http.post(Server.API_ENDPOINT +'getNotificationSize/', {
+      token: this.token
     },this.httpOptions)
   }
 
   seenNotification(notification: Notification) {
     this.http.post(Server.API_ENDPOINT +'seenNotification/', {
-      token: "mew",
+      token: this.token,
       id: notification.id
     },this.httpOptions)
   }
 
   sendDecisionOfNotification(notification: Notification, decision: string) {
     this.http.post(Server.API_ENDPOINT +'sendDecisionOfNotification/', {
-      token: "mew",
+      token: this.token,
       id: notification.id,
       decision: decision
     },this.httpOptions)
-  }
-
-  hi () {
-    return "hi";
   }
 }
