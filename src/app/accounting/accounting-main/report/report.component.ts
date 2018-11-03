2f7388fc9f3932ae06151d206b516609cf9a7f8a
Aquib Azmain <bsse0718@iit.du.ac.bd>
2018-11-03 21:25:00 +0600

    report date update

--- src/app/accounting/accounting-main/report/report.component.ts
+++ src/app/accounting/accounting-main/report/report.component.ts
@@ -11,7 +11,33 @@ export class ReportComponent implements OnInit {
   bsValue = new Date();
   constructor() { }
 
+  dateRange:any;
+  startDate:any;
+  endDate:any;
+
   ngOnInit() {
+  }
+
+  generateReport(){
+    this.startDate = this.formatDate(this.dateRange[0]);
+    this.endDate = this.formatDate(this.dateRange[1]);
+    console.log(this.startDate);
+    console.log(this.endDate);
+  }
+
+  public formatDate(date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+  
+    var day = date.getDate();
+    var monthIndex = date.getMonth()+1;
+    var year = date.getFullYear();
+  
+    return day + '/' + monthIndex + '/' + year;
   }
 
 }
