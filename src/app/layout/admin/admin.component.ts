549852b395b52b9942c28fa9c52c28d7227d9afe
Moumita Asad <bsse0731@iit.du.ac.bd>
2018-11-05 23:51:19 +0600

    employee and notification management finalized

--- src/app/layout/admin/admin.component.ts
+++ src/app/layout/admin/admin.component.ts
@@ -128,13 +128,14 @@ export class AdminComponent implements OnInit {
   }
 
   ngOnInit() {
-    // setInterval(() => {
-    //     this.notificationService.getNotificationSize()
-    //     .subscribe((response) => { 
-    //       console.log(response);
-    //       this.notificationSize = response['size'];
-    //     });
-    //   }, 5000);
+    this.notificationSize = 0;
+    setInterval(() => {
+        this.notificationService.getNotificationSize()
+        .subscribe((response) => { 
+          console.log(response);
+          this.notificationSize = response['size'];
+        });
+      }, 5000);
   }
 
   onResize(event) {
--- src/utils/Server.ts
+++ src/utils/Server.ts
@@ -1,3 +1,3 @@
 export class Server {
-    public static API_ENDPOINT='http://10.100.103.153:5000/api/';
+    public static API_ENDPOINT='http://172.16.20.105:9999/api/';
 }
